5508595106f7f15f636d359fc4ff4fcbfdc1150e
zhangsheng <zhsheng26@gmail.com>
2019-07-13 11:31:46 +0800

    menu

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,39 @@ class WeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "WeLook",
-        theme: ThemeData(primaryColor: Colors.deepOrange),
-        home: RandomWords());
+      title: "WeLook",
+      theme: ThemeData(primaryColor: Colors.deepOrange),
+      home: HomeMenu(),
+      routes: {
+        "randomWord": (context) => RandomWords(),
+      },
+    );
+  }
+}
+
+class HomeMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Flutter Summary")),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed("randomWord");
+                },
+                child: Text("RandomWord"),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("Learn"),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
